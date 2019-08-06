04dacd41b097989012a558754b9a0175926cf707
Matt Covalt <mcovalt@mailbox.org>
2019-08-06 16:32:45 -0400

    Update mappings

--- queries/dashboard.sql
+++ queries/dashboard.sql
@@ -1,37 +1,32 @@
 SELECT
-    'Chemical' as facet_model_name,
     rc.id as rawchem_id,
-    rc.raw_cas,
-    rc.raw_chem_name,
-    dss.sid as dtxsid,
-    dss.true_cas,
-    dss.true_chemname,
-    dd.id as data_document_id,
-    dd.title as data_document_title,
+    rc.raw_cas as rawchem_cas,
+    rc.raw_chem_name as rawchem_name,
+    dss.sid as truechem_dtxsid,
+    dss.true_cas as truechem_cas,
+    dss.true_chemname as truechem_name,
+    dd.id as datadocument_id,
+    gt.code as datadocument_grouptype,
+    dd.title as datadocument_title,
+    dd.subtitle as datadocument_subtitle,
+    p.id as product_id,
+    p.upc as product_upc,
+    p.manufacturer as product_manufacturer,
+    p.brand_name as product_brandname,
+    p.title as product_title,
     (SELECT
         CONCAT(
-            '/media/',
-            dg.fs_id,
-            '/pdf/document_',
-            dd.id,
-            '.',
-            (SELECT
-                SUBSTRING_INDEX(
-                    dd.filename, 
-                    '.',
-                    -1
-                )
-            )
+            p.short_description,
+            '\n\n',
+            p.long_description
         )
-    ) as data_document_pdf,
-    p.id as product_id,
-    p.title as product_title,
+    ) as product_description,
     puc.id as puc_id,
-    puc.gen_cat as puc_gen_cat,
-    puc.prod_fam as puc_prod_fam,
-    puc.prod_type as puc_prod_type,
-    puc.description as puc_description,
     puc.kind as puc_kind,
+    puc.gen_cat as puc_gencat,
+    puc.prod_fam as puc_prodfam,
+    puc.prod_type as puc_prodtype,
+    puc.description as puc_description,
     (SELECT
         UNIX_TIMESTAMP(
             GREATEST(
@@ -50,6 +45,7 @@ FROM
     INNER JOIN dashboard_extractedtext et ON rc.extracted_text_id = et.data_document_id
     INNER JOIN dashboard_datadocument dd ON et.data_document_id = dd.id
     INNER JOIN dashboard_datagroup dg ON dd.data_group_id = dg.id
+    INNER JOIN dashboard_grouptype gt ON dg.group_type_id = gt.id
     LEFT JOIN dashboard_dsstoxlookup dss ON rc.dsstox_id = dss.id
     LEFT JOIN dashboard_productdocument pd ON dd.id = pd.document_id
     LEFT JOIN dashboard_product p ON pd.product_id = p.id
@@ -59,6 +55,7 @@ WHERE
     dss.updated_at > :sql_last_value OR
     et.updated_at > :sql_last_value OR
     dd.updated_at > :sql_last_value OR
+    dg.updated_at > :sql_last_value OR
     pd.updated_at > :sql_last_value OR
     p.updated_at > :sql_last_value OR
     pp.updated_at > :sql_last_value OR
